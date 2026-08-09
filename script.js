1c49d0d74d40ebcc6859234000c283b34297a54a
aryagarudatimnas-creator <aryagarudatimnas@gmail.com>
2026-08-09 18:56:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ const pages = [
     "Tapi, selamat yaaa kin! Meskipun kita belum kenal disaat lu lagi ngelewatin proses panjang skripsi dan lain lain yang pastinya bikin lu muak dan stress, ijinin gue buat ngasih apresiasi sebesar-besarnya ke lu karna lu udah hebat banget ngelewatin masa itu ^^",
     "I know this is a little bit cliche, but I wanna say \"i'm so proud of u\". Gua harap kita bisa ketemu dan berbagi cerita langsung tanpa chattingan lagi.",
     "Apapun mimpi dan tujuan yang mau lu raih, gua harap lu tau pasti bahwa lu bisa dapetin hal itu. Karna lu adalah kikin, gua percaya lu pasti bisa.",
-    "Sehat selalu dan bahagia selalu yaa kin, gua harap kebahagiaan lu nantinya ada part dimana gua lah sumber kebahagiaan itu HAHAHAHAHAH just kidding <br><br><div class='bouquet-container'><img src='bunga.jpeg' alt='Virtual Bouquet' class='bouquet-img'></div><br><strong>Once again, happy graduation kikin!!</strong><br><br><div class='signature-box'><img src='addit.png' alt='Arya' class='profile-mini'><span>- Arya Rachman Praditya</span></div>"
+    "Sehat selalu dan bahagia selalu yaa kin, gua harap kebahagiaan lu nantinya ada part dimana gua lah sumber kebahagiaan itu HAHAHAHAHAH just kidding <br><br><div class='bouquet-container'><img src='bunga.png' alt='Virtual Bouquet' class='bouquet-img'></div><br><strong>Once again, happy graduation kikin!!</strong><br><br><div class='signature-box'><img src='addit.png' alt='Arya' class='profile-mini'><span>- Arya Rachman Praditya</span></div>"
 ];
 
 let currentPage = 0;
